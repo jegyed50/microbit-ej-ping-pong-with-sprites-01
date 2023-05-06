bc2f765c8c9bbd4d087e8f0468abecf12d1371ea
Egyed József <jegyed50@gmail.com>
2023-05-06 19:25:15 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,43 @@
 input.onButtonPressed(Button.A, function () {
-	
+    if (utoBal.get(LedSpriteProperty.X) > 0) {
+        utoBal.change(LedSpriteProperty.X, -1)
+        utoJobb.change(LedSpriteProperty.X, -1)
+    }
 })
-let utoBal = game.createSprite(2, 4)
-let utoJobb = game.createSprite(3, 4)
+input.onButtonPressed(Button.B, function () {
+    if (utoBal.get(LedSpriteProperty.X) < 3) {
+        utoBal.change(LedSpriteProperty.X, 1)
+        utoJobb.change(LedSpriteProperty.X, 1)
+    }
+})
+let utoJobb: game.LedSprite = null
+let utoBal: game.LedSprite = null
+utoBal = game.createSprite(2, 4)
+utoJobb = game.createSprite(3, 4)
 let labda = game.createSprite(randint(0, 4), 0)
 let Xirany = 1
 let Yirany = randint(-1, 1)
 basic.pause(500)
 basic.forever(function () {
-	
+    labda.change(LedSpriteProperty.X, Xirany)
+    labda.change(LedSpriteProperty.Y, Yirany)
+    if (labda.isTouching(utoBal) || labda.isTouching(utoJobb)) {
+        labda.change(LedSpriteProperty.X, Xirany * -1)
+        labda.change(LedSpriteProperty.Y, -1)
+    } else {
+        if (labda.get(LedSpriteProperty.Y) < 0) {
+            Xirany = 1
+            Xirany = randint(-1, 1)
+        } else if (labda.get(LedSpriteProperty.Y) > 4) {
+            labda.set(LedSpriteProperty.Blink, 1)
+            basic.pause(2000)
+            game.gameOver()
+        }
+        if (labda.get(LedSpriteProperty.X) < 0) {
+            Xirany = 1
+        } else if (labda.get(LedSpriteProperty.X) > 0) {
+            Xirany = -1
+        }
+        basic.pause(500)
+    }
 })
