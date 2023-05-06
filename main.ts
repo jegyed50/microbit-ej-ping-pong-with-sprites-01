8c334f72a5749ae6a8686c6d1e8b3d8f8f2bd1c1
Egyed József <jegyed50@gmail.com>
2023-05-06 19:07:14 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+let utoBal = game.createSprite(2, 4)
+let utoJobb = game.createSprite(3, 4)
+let labda = game.createSprite(randint(0, 4), 0)
+let Xirany = 1
+let Yirany = randint(-1, 1)
+basic.pause(500)
+basic.forever(function () {
+	
+})
